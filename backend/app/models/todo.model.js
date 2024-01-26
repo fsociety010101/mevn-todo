17b7a3b8c787129dcb2237c9b4fc10320af12d16
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2024-01-26 20:25:37 +0100

    overrided toJSON db method

--- backend/app/models/todo.model.js
+++ backend/app/models/todo.model.js
@@ -1,15 +1,19 @@
 module.exports = mongoose => {
-    const Todo = mongoose.model(
-        "todo",
-        mongoose.Schema(
-            {
-                title: String,
-                description: String,
-                published: Boolean
-            },
-            { timestamps: true }
-        )
+    var schema = mongoose.Schema(
+        {
+            title: String,
+            description: String,
+            published: Boolean
+        },
+        { timestamps: true }
     );
 
+    schema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Todo = mongoose.model("todo", schema);
     return Todo;
 };
