3240616251bd7ea79e30f2a1564b420133a2d7e3
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2024-01-26 19:38:36 +0100

    added todo addition component

---
+++ frontend/src/components/AddTodo.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="submit-form">
+        <div v-if="!submitted">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" id="title" required v-model="todo.title" name="title" />
+            </div>
+
+            <div class="form-group">
+                <label for="description">Description</label>
+                <input class="form-control" id="description" required v-model="todo.description" name="description" />
+            </div>
+
+            <button @click="savetodo" class="btn btn-success">Submit</button>
+        </div>
+
+        <div v-else>
+            <h4>You submitted successfully!</h4>
+            <button class="btn btn-success" @click="newtodo">Add</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoDataService from "../services/TodoDataService";
+
+export default {
+    name: "add-todo",
+    data() {
+        return {
+            todo: {
+                id: null,
+                title: "",
+                description: "",
+                published: false
+            },
+            submitted: false
+        };
+    },
+    methods: {
+        savetodo() {
+            var data = {
+                title: this.todo.title,
+                description: this.todo.description
+            };
+
+            TodoDataService.create(data)
+                .then(response => {
+                    this.todo.id = response.data.id;
+                    console.log(response.data);
+                    this.submitted = true;
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        },
+
+        newtodo() {
+            this.submitted = false;
+            this.todo = {};
+        }
+    }
+};
+</script>
+
+<style>
+.submit-form {
+    max-width: 300px;
+    margin: auto;
+}
+</style>
